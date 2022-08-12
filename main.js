6a04fd7da99750182710a897fc7350db4cb64a5b
brian rodrigo brown coopman <brianbrowncoopman@gmail.com>
2022-08-11 22:16:38 -0400

    finalizacion ejercicio

--- main.js
+++ main.js
@@ -3,45 +3,63 @@ formElement = document.querySelector("form"),
 listElement = document.querySelector("ul"),
 totalTask = document.getElementById("footer__total")
 
-let taskList = ["Aprender a Programar","Sacar al Perro"];
+let taskList = [];
+
+function deleteItem(e){
+  let task = e.target.parentElement.previousElementSibling.innerHTML
+
+  let index =taskList.indexOf(task);
+  if(index !== -1){
+    taskList.splice(index, 1);
+  }
+  lista()
+}
 
 function lista() {
     listElement.innerHTML = "";
     taskList.forEach((item) => {
         let newItem = document.createElement("li");
-
+//Agregar y inyectar el span a li
         let span = document.createElement("span");
         span.innerHTML = item;
         newItem.appendChild(span);
-
+//Crear y agregar el enlace y el boton eliminar a li
         let enlace = document.createElement("a");
         enlace.classList.add("delete");
         enlace.innerHTML =`<i class="fa-solid fa-trash-can"></i>`;
         newItem.appendChild(enlace);
+//Eliminar
+        enlace.addEventListener('click', e =>{
+          deleteItem(e)
+        })
 
         listElement.appendChild(newItem);
     });
 
     totalTask.innerHTML=taskList.length
 
-    /*inputElement.value = "";*/
+    
 }
 
 lista();
 
-/*function addTask() {
-  if (inputElement.value) {
+function whitoutSpace(e){
+  let string = e.trim()
+  return string.length > 0;
+}
+
+function addTask() {
+  if (inputElement.value && whitoutSpace(inputElement.value) && !taskList.includes(inputElement.value)) {
     taskList.push(inputElement.value);
     lista();
   }
-}*/
+  inputElement.value = "";
+}
 
-/*formElement.addEventListener('submit', (e) => {
+formElement.addEventListener('submit', (e) => {
   e.preventDefault();
   addTask();
-});*/
+});
 
 
 
-/*var myStr = "i am a \"double quoted\" string inside \"double quoted\".";
-console.log(myStr)*/
